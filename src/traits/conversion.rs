079512b8ec6a5e1e1f4b57c6103e1ba6b3a7b12f
MatteoH2O1999 <matteo.dellacqua2@studenti.unimi.it>
2024-12-13 13:45:24 +0100

    Add conversions for chunks

--- src/traits/conversion.rs
+++ src/traits/conversion.rs
@@ -6,4 +6,13 @@ pub trait ParSliceView<T> {
     fn as_data_race_par_slice(&mut self) -> impl UnsafeDataRaceParSlice<T>;
 
     fn as_unsafe_par_slice(&mut self) -> impl UnsafeParSlice<T>;
+
+    fn as_pointer_par_chunk_slice(&mut self, chunk_size: usize) -> impl PointerParSlice<[T]>;
+
+    fn as_data_race_par_chunk_slice(
+        &mut self,
+        chunk_size: usize,
+    ) -> impl UnsafeDataRaceParChunkSlice<T>;
+
+    fn as_unsafe_par_chunk_slice(&mut self, chunk_size: usize) -> impl UnsafeParSlice<[T]>;
 }
