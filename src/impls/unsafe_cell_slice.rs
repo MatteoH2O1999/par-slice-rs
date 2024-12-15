fde2438449ba21eb95a810f5d15567f56b0bfed0
MatteoH2O1999 <matteo.dellacqua2@studenti.unimi.it>
2024-12-15 23:34:41 +0100

    Impl unsafe slice

--- src/impls/unsafe_cell_slice.rs
+++ src/impls/unsafe_cell_slice.rs
@@ -1,7 +1,33 @@
-use crate::PointerParSlice;
-use std::{cell::UnsafeCell, mem::size_of};
+use crate::{ParSliceView, PointerParSlice};
+use std::{cell::UnsafeCell, mem::size_of, ops::Deref};
 
-unsafe impl<T> PointerParSlice<T> for UnsafeCell<[T]> {
+pub(crate) struct UnsafeCellSlice<B>(B);
+
+impl<T> UnsafeCellSlice<&UnsafeCell<[T]>> {
+    pub fn new_borrowed(slice: &mut [T]) -> Self {
+        // TODO: replace with UnsafeCell::from_mut when stable
+        let ptr = slice as *mut [T] as *mut UnsafeCell<[T]>;
+        let unsafe_slice = unsafe {
+            // Safety: UnsafeCell is repr(transparent)
+            &mut *ptr
+        };
+        Self(unsafe_slice)
+    }
+}
+
+impl<T> UnsafeCellSlice<Box<UnsafeCell<[T]>>> {
+    pub unsafe fn new_owned(ptr: *mut [T]) -> Self {
+        let ptr = ptr as *mut UnsafeCell<[T]>;
+        let boxed = unsafe {
+            // Safety: UnsafeCell is repr(transparent) and caller guarantees
+            // ownership of the allocation pointed to by ptr
+            Box::from_raw(ptr)
+        };
+        Self(boxed)
+    }
+}
+
+unsafe impl<T, B: Deref<Target = UnsafeCell<[T]>>> PointerParSlice<T> for UnsafeCellSlice<B> {
     #[inline(always)]
     fn get_ptr_unchecked(&self, index: usize) -> *const T {
         self.get_mut_ptr_unchecked(index) as *const T
@@ -12,7 +38,7 @@ unsafe impl<T> PointerParSlice<T> for UnsafeCell<[T]> {
         debug_assert!(index < self.len());
         debug_assert!(index * size_of::<T>() < isize::MAX as usize);
 
-        let mut ptr = self.get() as *mut T;
+        let mut ptr = self.0.get() as *mut T;
         unsafe {
             // Safety: ptr is derived from an allocated object so cannot be bigger
             // than isize::MAX bytes
@@ -23,6 +49,116 @@ unsafe impl<T> PointerParSlice<T> for UnsafeCell<[T]> {
 
     #[inline(always)]
     fn len(&self) -> usize {
-        self.get().len()
+        self.0.get().len()
+    }
+}
+
+pub(crate) struct UnsafeCellChunkSlice<B> {
+    inner: B,
+    len: usize,
+    chunk_size: usize,
+}
+
+impl<T> UnsafeCellChunkSlice<&UnsafeCell<[T]>> {
+    fn new_borrowed(slice: &mut [T], chunk_size: usize) -> Self {
+        assert_eq!(slice.len() % chunk_size, 0);
+        let len = slice.len() / chunk_size;
+
+        // TODO: replace with UnsafeCell::from_mut when stable
+        let ptr = slice as *mut [T] as *mut UnsafeCell<[T]>;
+        let unsafe_slice = unsafe {
+            // Safety: UnsafeCell is repr(transparent)
+            &mut *ptr
+        };
+        Self {
+            inner: unsafe_slice,
+            len,
+            chunk_size,
+        }
+    }
+}
+
+impl<T> UnsafeCellChunkSlice<Box<UnsafeCell<[T]>>> {
+    fn new_owned(ptr: *mut [T], chunk_size: usize) -> Self {
+        assert_eq!(ptr.len() % chunk_size, 0);
+        let len = ptr.len() / chunk_size;
+
+        let ptr = ptr as *mut UnsafeCell<[T]>;
+        let boxed = unsafe {
+            // Safety: UnsafeCell is repr(transparent) and caller guarantees
+            // ownership of the allocation pointed to by ptr
+            Box::from_raw(ptr)
+        };
+
+        Self {
+            inner: boxed,
+            len,
+            chunk_size,
+        }
+    }
+}
+
+unsafe impl<T, B: Deref<Target = UnsafeCell<[T]>>> PointerParSlice<[T]>
+    for UnsafeCellChunkSlice<B>
+{
+    #[inline(always)]
+    fn get_ptr_unchecked(&self, index: usize) -> *const [T] {
+        self.get_mut_ptr_unchecked(index) as *const [T]
+    }
+
+    #[inline(always)]
+    fn get_mut_ptr_unchecked(&self, index: usize) -> *mut [T] {
+        debug_assert!(index < self.len());
+
+        let offset = index * self.chunk_size;
+        debug_assert!(offset * size_of::<T>() < isize::MAX as usize);
+
+        let mut ptr = self.inner.get() as *mut T;
+        unsafe {
+            // Safety: ptr is derived from an allocated object so cannot be bigger
+            // than isize::MAX bytes
+            ptr = ptr.add(offset);
+        }
+        std::ptr::slice_from_raw_parts_mut(ptr, self.chunk_size)
+    }
+
+    #[inline(always)]
+    fn len(&self) -> usize {
+        self.len
+    }
+}
+
+impl<T> ParSliceView<T> for [T] {
+    #[inline(always)]
+    fn as_pointer_par_slice(&mut self) -> impl PointerParSlice<T> {
+        UnsafeCellSlice::new_borrowed(self)
+    }
+
+    #[inline(always)]
+    fn as_data_race_par_slice(&mut self) -> impl crate::UnsafeDataRaceParSlice<T> {
+        UnsafeCellSlice::new_borrowed(self)
+    }
+
+    #[inline(always)]
+    fn as_unsafe_par_slice(&mut self) -> impl crate::UnsafeParSlice<T> {
+        UnsafeCellSlice::new_borrowed(self)
+    }
+
+    #[inline(always)]
+    fn as_pointer_par_chunk_slice(&mut self, chunk_size: usize) -> impl PointerParSlice<[T]> {
+        UnsafeCellChunkSlice::new_borrowed(self, chunk_size)
+    }
+
+    #[inline(always)]
+    fn as_data_race_par_chunk_slice(
+        &mut self,
+        chunk_size: usize,
+    ) -> impl crate::UnsafeDataRaceParChunkSlice<T> {
+        UnsafeCellChunkSlice::new_borrowed(self, chunk_size)
+    }
+
+    #[inline(always)]
+    fn as_unsafe_par_chunk_slice(&mut self, chunk_size: usize) -> impl crate::UnsafeParSlice<[T]> {
+        UnsafeCellChunkSlice::new_borrowed(self, chunk_size)
     }
 }
--- src/traits/conversion.rs
+++ src/traits/conversion.rs
@@ -16,3 +16,20 @@ pub trait ParSliceView<T> {
 
     fn as_unsafe_par_chunk_slice(&mut self, chunk_size: usize) -> impl UnsafeParSlice<[T]>;
 }
+
+pub trait IntoParSlice<T> {
+    fn into_pointer_par_slice(self) -> impl PointerParSlice<T>;
+
+    fn into_data_race_par_slice(self) -> impl UnsafeDataRaceParSlice<T>;
+
+    fn into_unsafe_par_slice(self) -> impl UnsafeParSlice<T>;
+
+    fn into_pointer_par_chunk_slice(self, chunk_size: usize) -> impl PointerParSlice<[T]>;
+
+    fn into_data_race_par_chunk_slice(
+        self,
+        chunk_size: usize,
+    ) -> impl UnsafeDataRaceParChunkSlice<T>;
+
+    fn into_unsafe_par_chunk_slice(self, chunk_size: usize) -> impl UnsafeParSlice<[T]>;
+}
