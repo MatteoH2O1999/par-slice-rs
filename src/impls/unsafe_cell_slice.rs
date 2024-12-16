633fafad83d9d6b282f17568a18f00a1212fc013
MatteoH2O1999 <matteo.dellacqua2@studenti.unimi.it>
2024-12-16 17:28:42 +0100

    Simplify pointer operations

--- src/impls/unsafe_cell_slice.rs
+++ src/impls/unsafe_cell_slice.rs
@@ -51,13 +51,12 @@ unsafe impl<T, B: Deref<Target = UnsafeCell<[T]>>> PointerParSlice<T> for Unsafe
         debug_assert!(index < self.len());
         debug_assert!(index * size_of::<T>() < isize::MAX as usize);
 
-        let mut ptr = self.0.get() as *mut T;
+        let ptr = self.0.get() as *mut T;
         unsafe {
             // Safety: ptr is derived from an allocated object so cannot be bigger
             // than isize::MAX bytes
-            ptr = ptr.add(index);
+            ptr.add(index)
         }
-        ptr
     }
 
     #[inline(always)]
