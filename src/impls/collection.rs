66e91a776ceef754be98426b6a76454ee2ff401e
MatteoH2O1999 <matteo.dellacqua2@studenti.unimi.it>
2025-01-12 23:13:39 +0100

    Add collection impls for arrays

--- src/impls/collection.rs
+++ src/impls/collection.rs
@@ -23,3 +23,49 @@ unsafe impl<T> TrustedSizedCollection for [T] {
         self.is_empty()
     }
 }
+
+unsafe impl<T, const N: usize> TrustedSizedCollection for [T; N] {
+    #[inline(always)]
+    fn len(&self) -> usize {
+        N
+    }
+
+    #[inline(always)]
+    fn is_empty(&self) -> bool {
+        N == 0
+    }
+}
+
+unsafe impl<T, const N: usize> TrustedChunkSizedCollection for [[T; N]] {
+    #[inline(always)]
+    fn chunk_size(&self) -> usize {
+        N
+    }
+
+    #[inline(always)]
+    fn num_chunks(&self) -> usize {
+        self.len()
+    }
+
+    #[inline(always)]
+    fn num_elements(&self) -> usize {
+        self.len() * N
+    }
+}
+
+unsafe impl<T, const N: usize, const M: usize> TrustedChunkSizedCollection for [[T; N]; M] {
+    #[inline(always)]
+    fn chunk_size(&self) -> usize {
+        N
+    }
+
+    #[inline(always)]
+    fn num_chunks(&self) -> usize {
+        M
+    }
+
+    #[inline(always)]
+    fn num_elements(&self) -> usize {
+        M * N
+    }
+}
