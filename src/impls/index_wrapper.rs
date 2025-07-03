de85d317270c7662d3b231af37d7f4bae01646d9
MatteoH2O1999 <matteo.dellacqua2@studenti.unimi.it>
2025-07-04 01:41:01 +0200

    Impl no ref index for wrapper

--- src/impls/index_wrapper.rs
+++ src/impls/index_wrapper.rs
@@ -1,11 +1,35 @@
 use crate::*;
 
+macro_rules! wrapper_method_doc {
+    ($method:path) => {
+        concat!(
+            "Semantically equivalent to [`",
+            stringify!($method),
+            "`](`",
+            stringify!($method),
+            "`)`(index.`[`as_usize`](`AsUsize::as_usize`)`())`."
+        )
+    };
+    ($method:path,$other_params:literal) => {
+        concat!(
+            "Semantically equivalent to [`",
+            stringify!($method),
+            "`](`",
+            stringify!($method),
+            "`)`(index.`[`as_usize`](`AsUsize::as_usize`)`()",
+            $other_params,
+            ")`."
+        )
+    };
+}
+
 pub struct IndexWrapper<I, T: ?Sized, B> {
     backend: B,
     _marker: std::marker::PhantomData<(I, T)>,
 }
 
 impl<T: ?Sized, B: ParView<T>> IndexWrapper<(), T, B> {
+    #[inline]
     pub fn new<I: AsUsize>(collection: B) -> IndexWrapper<I, T, B> {
         IndexWrapper {
             backend: collection,
@@ -22,35 +46,105 @@ impl<I, T, B> IndexWrapper<I, T, B> {
 }
 
 impl<I: AsUsize, T: ?Sized, B: PointerIndex<T>> IndexWrapper<I, T, B> {
-    /// Semantically equivalent to
-    /// [`PointerIndex::get_ptr`](`PointerIndex::get_ptr`)`(i)` where `i` is
-    /// `index.`[`as_usize`](`AsUsize::as_usize`).
+    #[doc = wrapper_method_doc!(PointerIndex::get_ptr)]
     #[inline]
     pub fn get_ptr(&self, index: I) -> *const T {
         self.backend.get_ptr(index.as_usize())
     }
 
-    /// Semantically equivalent to
-    /// [`PointerIndex::get_ptr_unchecked`](`PointerIndex::get_ptr_unchecked`)`(i)` where `i` is
-    /// `index.`[`as_usize`](`AsUsize::as_usize`).
+    #[doc = wrapper_method_doc!(PointerIndex::get_ptr_unchecked)]
     #[inline]
     pub unsafe fn get_ptr_unchecked(&self, index: I) -> *const T {
         unsafe { self.backend.get_ptr_unchecked(index.as_usize()) }
     }
 
-    /// Semantically equivalent to
-    /// [`PointerIndex::get_mut_ptr`](`PointerIndex::get_mut_ptr`)`(i)` where `i` is
-    /// `index.`[`as_usize`](`AsUsize::as_usize`).
+    #[doc = wrapper_method_doc!(PointerIndex::get_mut_ptr)]
     #[inline]
     pub fn get_mut_ptr(&self, index: I) -> *mut T {
         self.backend.get_mut_ptr(index.as_usize())
     }
 
-    /// Semantically equivalent to
-    /// [`PointerIndex::get_mut_ptr_unchecked`](`PointerIndex::get_mut_ptr_unchecked`)`(i)` where `i` is
-    /// `index.`[`as_usize`](`AsUsize::as_usize`).
+    #[doc = wrapper_method_doc!(PointerIndex::get_mut_ptr_unchecked)]
     #[inline]
     pub unsafe fn get_mut_ptr_unchecked(&self, index: I) -> *mut T {
         unsafe { self.backend.get_mut_ptr_unchecked(index.as_usize()) }
+    }
+}
+
+impl<I: AsUsize, T, B: UnsafeNoRefIndex<T>> IndexWrapper<I, T, B> {
+    #[doc = wrapper_method_doc!(UnsafeNoRefIndex::get_value)]
+    #[inline]
+    pub unsafe fn get_value(&self, index: I) -> T
+    where
+        T: Copy,
+    {
+        unsafe { self.backend.get_value(index.as_usize()) }
+    }
+
+    #[doc = wrapper_method_doc!(UnsafeNoRefIndex::get_value_unchecked)]
+    #[inline]
+    pub unsafe fn get_value_unchecked(&self, index: I) -> T
+    where
+        T: Copy,
+    {
+        unsafe { self.backend.get_value_unchecked(index.as_usize()) }
+    }
+
+    #[doc = wrapper_method_doc!(UnsafeNoRefIndex::set_value, ", value")]
+    #[inline]
+    pub unsafe fn set_value(&self, index: I, value: T) {
+        unsafe {
+            self.backend.set_value(index.as_usize(), value);
+        }
+    }
+
+    #[doc = wrapper_method_doc!(UnsafeNoRefIndex::set_value_unchecked, ", value")]
+    #[inline]
+    pub unsafe fn set_value_unchecked(&self, index: I, value: T) {
+        unsafe {
+            self.backend.set_value_unchecked(index.as_usize(), value);
+        }
+    }
+}
+
+impl<I: AsUsize, T, B: UnsafeNoRefChunkIndex<T>> IndexWrapper<I, T, B> {
+    #[doc = wrapper_method_doc!(UnsafeNoRefChunkIndex::get_values, ", out")]
+    #[inline]
+    pub unsafe fn get_values<O: AsMut<[T]>>(&self, index: I, out: O) -> O
+    where
+        T: Copy,
+    {
+        unsafe { self.backend.get_values(index.as_usize(), out) }
+    }
+
+    #[doc = wrapper_method_doc!(UnsafeNoRefChunkIndex::get_values_unchecked, ", out")]
+    #[inline]
+    pub unsafe fn get_values_unchecked<O: AsMut<[T]>>(&self, index: I, out: O) -> O
+    where
+        T: Copy,
+    {
+        unsafe { self.backend.get_values_unchecked(index.as_usize(), out) }
+    }
+
+    #[doc = wrapper_method_doc!(UnsafeNoRefChunkIndex::set_values, ", values")]
+    #[inline]
+    pub unsafe fn set_values(&self, index: I, values: &[T])
+    where
+        T: Clone,
+    {
+        unsafe {
+            self.backend.set_values(index.as_usize(), values);
+        }
+    }
+
+    #[doc = wrapper_method_doc!(UnsafeNoRefChunkIndex::set_values_unchecked, ", values")]
+    #[inline]
+    pub unsafe fn set_values_unchecked(&self, index: I, values: &[T])
+    where
+        T: Clone,
+    {
+        unsafe {
+            self.backend.set_values_unchecked(index.as_usize(), values);
+        }
     }
 }
