eb0ad53f0533938c3ac6b205a03085d42c6d14e4
Matteo Dell'Acqua <82184604+MatteoH2O1999@users.noreply.github.com>
2025-08-11 00:34:15 +0200

    Finish docs for AsUsize trait

--- src/traits/indexing.rs
+++ src/traits/indexing.rs
@@ -1,3 +1,16 @@
+/// Convert non indexing types to be used for unsafe idexing with
+/// traits [`PointerIndex`](crate::PointerIndex), [`UnsafeNoRefIndex`](crate::UnsafeNoRefIndex)
+/// and [`UnsafeIndex`](crate::UnsafeIndex) thanks to [`IndexWrapper`](crate::IndexWrapper).
+///
+/// # Safety
+/// It must hold that `x != y <=> x.as_usize() != y.as_usize()`.
+///
+/// [`as_usize`](AsUsize::as_usize) may panic if `x` has no image in the codomain [`usize`].
 pub unsafe trait AsUsize {
+    /// Converts the input type into the indexing type [`usize`].
+    ///
+    /// # Panics
+    ///
+    /// Panics if `self` has no image of type [`usize`].
     fn as_usize(&self) -> usize;
 }
